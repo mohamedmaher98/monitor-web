2e37058d1a4de3d538c58448eb3db88e251621b9
maher <mohamdmaher1998@gmail.com>
2026-09-22 13:09:44 +0300

    adding the logger system and the log level enum

--- src/main/java/uitl/AppConfigUtil.java
+++ src/main/java/uitl/AppConfigUtil.java
@@ -2,6 +2,9 @@ package uitl;
 
 import java.io.PrintWriter;
 
+import util.LogLevel;
+import util.LogUtil;
+
 import schedul.WeatherPollerTimerManager;
 
 public class AppConfigUtil {
@@ -13,15 +16,18 @@ public class AppConfigUtil {
 			seconds = Integer.valueOf(s);
 			if (seconds < 30) {
 				writer.print("Seconds Cannot be less than 30 sec");
+				LogUtil.log(LogLevel.ERROR, "Seconds Cannot be less than 30 sec", AppConfigUtil.class);
 				return;
 			}
 		} catch (Exception e) {
 			writer.print("Invalid Input For Seconds");
+			LogUtil.log(LogLevel.ERROR, "Invalid Input For Seconds", AppConfigUtil.class);
 			return;
 
 		}
 		WeatherPollerTimerManager.start(seconds);
 		writer.print("the service started successfully");
+		LogUtil.log(LogLevel.INFO, "the service started successfully", AppConfigUtil.class);
 
 	}
 
@@ -32,15 +38,17 @@ public class AppConfigUtil {
 			seconds = Integer.valueOf(s);
 			if (seconds <= 30) {
 				seconds = 30;
-				System.out.println("this value is the default value because the value from the db is less than 30");
+				LogUtil.log(LogLevel.ERROR,"this value is the default value because the value from the db is less than 30", AppConfigUtil.class);
+
 			}
 			WeatherPollerTimerManager.start(seconds);
 			writer.print("the service started successfully");
+			LogUtil.log(LogLevel.INFO, "the service started successfully", AppConfigUtil.class);
 		} catch (Exception e) {
 			WeatherPollerTimerManager.start(30);
 			writer.print("the service started successfully");
-			System.out.println(e.getMessage() + "the value from the db is: " + s
-					+ "the service is running now with deafult time poll ");
+			LogUtil.log(LogLevel.ERROR, e.getMessage() + "the value from the db is: " + s
+					+ "the service is running now with deafult time poll ", AppConfigUtil.class);
 
 		}
 
